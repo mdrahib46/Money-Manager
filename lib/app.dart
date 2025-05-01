42bd2d728c4bc85d9f5249ef1a2baf3831d224fc
mdrahib46 <nahid179846@bsdi-bd.org>
2025-05-01 21:48:19 +0600

    Login page has been implemented

--- lib/app.dart
+++ lib/app.dart
@@ -6,6 +6,25 @@ class ExpenseTracker extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Splashscreen());
+    return MaterialApp(
+      theme: ThemeData(
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: TextStyle(color: Colors.grey, letterSpacing: 1.2),
+          fillColor: Color(0xFFF7F8F9),
+          filled: true,
+          labelStyle: const TextStyle(color: Colors.grey),
+          suffixIconColor: Colors.grey,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(color: Color(0xFFE8ECF4)),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(color: Color(0xFFE8ECF4)),
+          ),
+        ),
+      ),
+      home: Splashscreen(),
+    );
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:moneytracker/widgets/app_name_logo.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AppNameLogoWidget(),
+              const SizedBox(height: 8),
+              Text(
+                "Welcome back! Glad to see you again, Again",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 24),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Enter your  email"),
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Enter your password"),
+              ),
+              Align(
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text('Forgot password'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
